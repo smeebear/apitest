e97885d2878e11319f224b8bb2277e777f432261
SmeeBear <soheron@gmail.com>
2020-10-04 20:38:12 -0500

    Initial commit, API seems to be working

---
+++ server.js
@@ -0,0 +1,13 @@
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+var port = 9000;
+
+app.use(bodyParser.json());
+
+app.get("/api/placeholder", (req, res) => {
+    const output = "Hi this is Sam O";
+    res.send({"outputText" : output});
+});
+
+app.listen(port, () => console.log("API running on port 9000"));
